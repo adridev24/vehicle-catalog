ee785d7cd57edafed55940baf8ffc6d14f86746d
Adriana <159927748+adridev24@users.noreply.github.com>
2026-04-13 17:30:29 -0300

    Create VehicleDetail component

---
+++ frontend/src/pages/VehicleDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VehicleDetail = () => {
+    return (
+        <div>
+            <h1>Vehicle Detail Page</h1>
+            {/* Additional vehicle details can be displayed here */}
+        </div>
+    );
+};
+
+export default VehicleDetail;
